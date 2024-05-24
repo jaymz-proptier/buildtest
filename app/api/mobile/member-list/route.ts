a8a7bd935b1057464c3c85e9997fcab2eff5949d
Jaymz <jaymz@ekms.kr>
2024-05-24 10:54:47 +0900

    회원관리시작일,종료일

--- app/api/mobile/member-list/route.ts
+++ app/api/mobile/member-list/route.ts
@@ -22,7 +22,7 @@ export async function GET(req: NextRequest) {
         const countSql = `select count(*) as count from tb_data_member where useYn = 'Y'${sqlWhere}`;
         const countResult = await executeQuery(countSql, paramsArray) as unknown[];
         const totalCount = JSON.parse(JSON.stringify(countResult));
-        console.log("formData.page",searchParams,searchParams.has("product_type"));
+        //console.log("formData.page",searchParams,searchParams.has("product_type"));
         const sql = `select memSeq, 회원번호, 상호명, 대표자명, 휴대폰, concat(시도, ' ', (case when 시군구 = '세종시' then '' else 시군구 end), ' ',  읍면동, ' ', 상세주소) as 주소, 시도, 시군구, 상품명, 계약구분, 결제일, DATE_FORMAT(시작일, '%y.%m.%d') as 시작일, DATE_FORMAT(종료일, '%y.%m.%d') as 종료일, 상태, 계약전송수, 전송수 from tb_data_member where useYn = 'Y'${sqlWhere} limit ?, 10`;
         const result = await executeQuery(sql, [...paramsArray, ((Number(searchParams.get("page") ? searchParams.get("page") : 1) - 1) * 10)]) as unknown[];
         
