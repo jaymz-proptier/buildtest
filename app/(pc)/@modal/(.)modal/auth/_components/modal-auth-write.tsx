4d25b44a1e877737ad067d41d129483ddfb6eb24
Jaymz <jaymz@ekms.kr>
2024-05-26 10:47:25 +0900

    계정소속

--- app/(pc)/@modal/(.)modal/auth/_components/modal-auth-write.tsx
+++ app/(pc)/@modal/(.)modal/auth/_components/modal-auth-write.tsx
@@ -19,7 +19,7 @@ export default function AuthWrite({ data, me, searchParams }: { data: any, me: a
     
     const [sosok, setSosok] = useState(data?.sosok || "컨설턴트");  
     const {data: jojikCenter, isLoading, error} = useQuery<Item, Object, Item, [_1: string, _2: string]>({
-        queryKey: ['jojikCenter', data?.sosok ? "컨설턴트" : data?.sosok],
+        queryKey: ['jojikCenter', sosok ? sosok : "컨설턴트"],
         queryFn: getJojikCenter,
         staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
         gcTime: 300 * 1000,
@@ -36,17 +36,17 @@ export default function AuthWrite({ data, me, searchParams }: { data: any, me: a
     const [modify, setModify] = useState(data?.sawonCode ? false :true);
     const [openSelectBox, setOpenSelectBox] = useState<string | null>(null);
     const selectBoxRef = useRef<HTMLDivElement | null>(null);
-    const [centerCode, setCenterCode] = useState(data?.centerCode ? data.centerCode : jojikCenter?.data[0].code);
+    const [centerCode, setCenterCode] = useState(data?.centerCode ? data.centerCode : jojikCenter?.data[0]?.code);
     const [partCode, setPartCode] = useState<any | []>([]);
-    const [jojikCode, setJojikCode] = useState(data?.jojikCode ? data.jojikCode : `${jojikCenter?.data[0].code}1`);
-    const [jobCode, setJobCode] = useState(data?.jobCode ? Number(data.jobCode) : jobCodeList?.data[0].code);
+    const [jojikCode, setJojikCode] = useState(data?.jojikCode ? data.jojikCode : `${jojikCenter?.data[0]?.code}1`);
+    const [jobCode, setJobCode] = useState(data?.jobCode ? Number(data.jobCode) : jobCodeList?.data[0]?.code);
     const [swId, setSwId] = useState(data?.swId || "");
     const [swPwd, setSwPwd] = useState("");
     const [name, setName] = useState(data?.name || "");
     const router = useRouter();
 
     const {data: centerPartData, isLoading: isCenterPartDataLoading} = useQuery<Item, Object, Item, [_1: string, _2: string]>({
-        queryKey: ['centerPart', centerCode ? centerCode : jojikCenter?.data[0].code],
+        queryKey: ['centerPart', centerCode ? centerCode : jojikCenter?.data[0]?.code],
         queryFn: getCenterPart,
         staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
         gcTime: 300 * 1000,
@@ -100,9 +100,9 @@ export default function AuthWrite({ data, me, searchParams }: { data: any, me: a
         router.back();
     }
     useEffect(() => {
-        setCenterCode(data?.centerCode ? data.centerCode : jojikCenter?.data[0].code);
+        setCenterCode(data?.centerCode ? data.centerCode : jojikCenter?.data[0]?.code);
         setPartCode(centerPartData?.data);
-    }, [jojikCenter]);
+    }, [sosok, jojikCenter]);
     useEffect(() => {
         if(!isCenterPartDataLoading && centerPartData?.data) {
             //if(centerPartData?.data.length > 0 ) {
@@ -111,7 +111,7 @@ export default function AuthWrite({ data, me, searchParams }: { data: any, me: a
             //}
             setJojikCode(jojikCode ? jojikCode : centerPartData?.data[0]?.code);
         }
-    }, [sosok, centerCode, isCenterPartDataLoading, centerPartData]);
+    }, [centerCode, isCenterPartDataLoading, centerPartData]);
     useEffect(() => {
         setJobCode(data?.jobCode ? Number(data.jobCode) : jobCodeList?.data[0]?.code);
     }, [jobCodeList]);
