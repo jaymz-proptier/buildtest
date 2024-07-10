d4a96a7e25c7c88b93f18458592ad2acadab9c2f
Jaymz <jaymz@ekms.kr>
2024-07-10 10:48:32 +0900

    coalesce

--- app/api/pc/upload-view/route.ts
+++ app/api/pc/upload-view/route.ts
@@ -8,12 +8,12 @@ export async function GET(req: NextRequest) {
     try {
         
         const sql = `select a.*, (case when a.statusGubun = 'W' then '처리대기' when a.statusGubun = 'Y' then '처리완료' when a.statusGubun = 'N' then '처리실패' when a.statusGubun = 'D' then '삭제' end) as statusGubunName, (case when a.dataGubun = '4' then concat('신규: ',
-        (select count(uploadSeq) from tb_data_newcontracts where a.upchaSeq = upchaSeq and useYn = 'Y'), '건 / 만기: ', 
-        (select count(uploadSeq) from tb_data_expirecontracts where a.upchaSeq = upchaSeq and useYn = 'Y'), '건') 
+        coalesce((select count(uploadSeq) from tb_data_newcontracts where a.upchaSeq = upchaSeq and useYn = 'Y'), 0), '건 / 만기: ', 
+        coalesce((select count(uploadSeq) from tb_data_expirecontracts where a.upchaSeq = upchaSeq and useYn = 'Y'), 0), '건') 
         when a.dataGubun = '3' then concat('정산: ',
-        (select count(uploadSeq) from tb_data_calculate where a.upchaSeq = upchaSeq and useYn = 'Y'), '건 / 매출: ', 
-        (select count(uploadSeq) from tb_data_calculate_sales where a.upchaSeq = upchaSeq and useYn = 'Y'), '건 / 기타: ', 
-        (select count(uploadSeq) from tb_data_calculate_etc where a.upchaSeq = upchaSeq and useYn = 'Y'), '건') else '' end) as resultStatus from tb_upload_log a where a.upchaSeq = ?`;
+        coalesce((select count(uploadSeq) from tb_data_calculate where a.upchaSeq = upchaSeq and useYn = 'Y'), 0), '건 / 매출: ', 
+        coalesce((select count(uploadSeq) from tb_data_calculate_sales where a.upchaSeq = upchaSeq and useYn = 'Y'), 0), '건 / 기타: ', 
+        coalesce((select count(uploadSeq) from tb_data_calculate_etc where a.upchaSeq = upchaSeq and useYn = 'Y'), 0), '건') else '' end) as resultStatus from tb_upload_log a where a.upchaSeq = ?`;
         const result = await executeQuery(sql, [searchParams.get("upchaSeq")]) as unknown[];
         
         return NextResponse.json({ status: "OK", data: result[0] });
